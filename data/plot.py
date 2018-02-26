6fbbbb110d300b16f1af3f519fa683c80a7114f2
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-02-26 00:25:10 +0000

    Plot time since last message

--- data/plot.py
+++ data/plot.py
@@ -17,6 +17,7 @@ with open(args.LOG_FORMATED) as f:
         rssis.append(rssi)
         times.append(time)
 
+# remove measurements with invalid times
 i = 1
 while i < len(times):
     if times[i] < times[i-1]:
@@ -25,20 +26,26 @@ while i < len(times):
     else:
         i += 1
 
-#i = 0
-#c = 0
-#d = 1000
-#while i < len(times):
-#    if times[i] > c + d:
-#        c = times[i]
-#        print(c)
-#    i += 1
+# calc time since last message
+diffs = []
+mnt = min(times)
+mxt = max(times)
+ts = range(mnt, mxt+1)
+last = times[0]
+i = 0
+j = 1
+while i < len(ts):
+    if ts[i] > times[j]:
+        last = times[j]
+        j += 1
+    diffs.append(ts[i] - last)
+    i += 1
+plt.plot(ts, diffs, 'g-')
+plt.title('Time since last message')
+plt.xlabel('time [ms]')
+plt.ylabel('time since last message [ms]')
+plt.show()
 
-#rssis = rssis[10:588]
-#times = times[10:588]
-#print(len(times), len(rssis))
-#plt.plot(rssis)
-#plt.plot(times)
 plt.plot(times, rssis, 'ro-')
 plt.title('RSSI')
 plt.xlabel('time [ms]')
